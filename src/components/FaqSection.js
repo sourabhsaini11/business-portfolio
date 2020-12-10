85d2cba1299a82637afbe8859684090f81aeda4a
sourabhsaini11 <ss1841620@gmail.com>
2020-12-10 14:36:04 +0530

    4.FaqSection

---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const FaqSection = () => {
+    return (
+            <div className="faq">
+                <h2>AnyQuestions ? <span>FAQ</span></h2>
+                <div className="question">
+                    <h4>How do I Start?</h4>
+                    <div className="answer">
+                        <p>asd sdsad fdgf hjgj</p>
+                        <p>asdsa qwew ret vcb zcx gh ojj hbhbu huv klmklftctrtr n mjn fvf</p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>Daily Schedule</h4>
+                    <div className="answer">
+                        <p>asd sdsad fdgf hjgj</p>
+                        <p>asdsa qwew ret vcb zcx gh ojj hbhbu huv klmklftctrtr n mjn fvf</p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>Payment Methods</h4>
+                    <div className="answer">
+                        <p>asd sdsad fdgf hjgj</p>
+                        <p>asdsa qwew ret vcb zcx gh ojj hbhbu huv klmklftctrtr n mjn fvf</p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>What products do you offer?</h4>
+                    <div className="answer">
+                        <p>asd sdsad fdgf hjgj</p>
+                        <p>asdsa qwew ret vcb zcx gh ojj hbhbu huv klmklftctrtr n mjn fvf</p>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
 import ServiceSection from '../components/ServiceSection';
+import FaqSection from '../components/FaqSection';
 
 const AboutUs = () => {
     return(
         <>
         <AboutSection />
         <ServiceSection />
+        <FaqSection />
         </>
     )
 }
