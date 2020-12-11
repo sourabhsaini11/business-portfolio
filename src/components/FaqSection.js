7be6b792e6f5b868e7c41529db965c1c60f0a2c3
sourabhsaini11 <ss1841620@gmail.com>
2020-12-11 15:23:39 +0530

    8.Nav.React-Router

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import Styled from 'styled-components';
 import {About} from '../styles';
 
 const FaqSection = () => {
---
+++ src/pages/OurWork.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurWork = () => {
+    return(
+        <div>
+        <h1>dsdad</h1>
+        </div>
+    );
+};
+
+export default OurWork;
