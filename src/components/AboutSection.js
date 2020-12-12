dcd7e1578b2a3663a662ec8674028d403a862604
sourabhsaini11 <ss1841620@gmail.com>
2020-12-12 12:25:45 +0530

    14.MainPageAnimation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,8 @@ import home1 from '../img/home1.png';
 //import styled from 'styled-components'; 
 import {About, Hide, Description, Image} from '../styles';
 import { motion} from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
+
 
 
 const AboutSection = () => {
@@ -12,20 +14,20 @@ const AboutSection = () => {
             <Description>
                 <motion.div className="title">
                     <Hide>
-                    <motion.h2>We work to make</motion.h2>
+                    <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>your <span>Dreams</span> come</motion.h2>
+                        <motion.h2 variants={titleAnim}>your <span>Dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>true.</motion.h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact us for any Website or Application ideas that you have.We have professionals with amazing skills.</p>
-                <button>Contact Us</button>
+                <motion.p variants={fade}>Contact us for any Website or Application ideas that you have.We have professionals with amazing skills.</motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="img" /> 
+                <motion.img variants={photoAnim} src={home1} alt="img" /> 
             </Image>
         </About>
     )
