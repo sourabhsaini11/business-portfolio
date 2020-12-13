337d4ba62288617846f6e2b2fc8d06fa76a84345
sourabhsaini11 <ss1841620@gmail.com>
2020-12-13 12:37:35 +0530

    useInView

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -3,10 +3,13 @@ import styled from 'styled-components';
 import {About} from '../styles';
 import Toggle  from "./Toggle";
 import { AnimateSharedLayout } from 'framer-motion';
+import { scrollReveal } from "../animation";
+import { useScroll } from "./useScroll";
 
 const FaqSection = () => {
+    const [element, controls ]= useScroll();
     return (
-            <Faq>
+            <Faq variants={scrollReveal} animate={controls} initial="hidden" ref={element}>
                 <h2>AnyQuestions ? <span>FAQ</span></h2>
                 <AnimateSharedLayout>
                 <Toggle title="How do I Start?">
